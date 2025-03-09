5895e0afaf3d657a18a0ed365d4374a820b10361
Telmo Agostinho <129296049+telminator@users.noreply.github.com>
2025-03-09 23:08:18 +0000

    Added mock/sample data to list item preview

--- app/src/main/java/com/example/leboncoinalbumlibrary/presentation/components/AlbumListItem.kt
+++ app/src/main/java/com/example/leboncoinalbumlibrary/presentation/components/AlbumListItem.kt
@@ -11,11 +11,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.compose.ui.unit.dp
+import com.example.leboncoinalbumlibrary.domain.model.Album
 
 @Preview
 @Composable
-fun AlbumListItem(){
+fun AlbumListItem(@PreviewParameter(SampleAlbumProvider::class) album: Album){
     Card(
         modifier = Modifier
             .fillMaxWidth())
@@ -31,11 +34,25 @@ fun AlbumListItem(){
             }
 
             Text(
-                text = "Test Title",
+                text = album.title,
                 style = MaterialTheme.typography.titleMedium,
                 maxLines = 2,
                 modifier = Modifier.padding(8.dp)
             )
         }
     }
+}
+
+
+class SampleAlbumProvider: PreviewParameterProvider<Album> {
+    override val values: Sequence<Album>
+        get() = sequenceOf(
+            Album(
+                albumId = 1,
+                id = 2,
+                title = "accusamus beatae ad facilis cum similique qui sunt",
+                url = "https://placehold.co/600x600/771796/white/png",
+                thumbnailUrl = "https://placehold.co/150x150/771796/white/png"
+            )
+        )
 }
