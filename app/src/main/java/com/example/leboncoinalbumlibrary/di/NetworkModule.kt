7b7b659e921f9d0b55930ac9aaff5c9a25276ade
Telmo Agostinho <129296049+telminator@users.noreply.github.com>
2025-03-11 01:35:46 +0000

    Added User_agent interceptor to add header to Coil requests

--- app/src/main/java/com/example/leboncoinalbumlibrary/di/NetworkModule.kt
+++ app/src/main/java/com/example/leboncoinalbumlibrary/di/NetworkModule.kt
@@ -1,11 +1,18 @@
 package com.example.leboncoinalbumlibrary.di
 
+import android.content.Context
+import coil.Coil
+import coil.ImageLoader
 import com.example.leboncoinalbumlibrary.data.remote.api.AlbumApiService
+import com.example.leboncoinalbumlibrary.di.interceptor.UserAgentInterceptor
 import com.example.leboncoinalbumlibrary.util.Constants
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -27,5 +34,45 @@ object NetworkModule {
     @Singleton
     fun provideAlbumApiService(retrofit: Retrofit): AlbumApiService {
         return retrofit.create(AlbumApiService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideUserAgentInterceptor(): UserAgentInterceptor {
+        return UserAgentInterceptor()
+    }
+
+    @Provides
+    @Singleton
+    fun provideImageLoader(
+        @ApplicationContext context: Context,
+        userAgentInterceptor: UserAgentInterceptor
+    ): ImageLoader {
+        return ImageLoader.Builder(context)
+            .okHttpClient {
+                OkHttpClient.Builder()
+                    .addInterceptor(userAgentInterceptor)
+                    .build()
+            }
+            .crossfade(true)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun initializeCoil(imageLoader: ImageLoader): Boolean {
+        Coil.setImageLoader(imageLoader)
+        return true
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(userAgentInterceptor: UserAgentInterceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(userAgentInterceptor)
+            .addInterceptor(HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.HEADERS
+            })
+            .build()
     }
 }
